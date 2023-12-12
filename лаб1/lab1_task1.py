32d4bbbb3cd9c8c7961d467f6f98f2761f98dec8
LebedevaOlga <153619980+LebedevaOlga@users.noreply.github.com>
2023-12-13 02:25:30 +0300

    Add files via upload

---
+++ лаб1/lab1_task1.py
@@ -0,0 +1,8 @@
+numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
+
+num1 = numbers[:4] + numbers[5:]
+res1 = sum(num1)
+res2 = len(numbers)
+res3 = res1 / res2
+numbers[4] = res3
+print("Измененный список:", numbers)
---
+++ лаб1/lab1_task2.py
@@ -0,0 +1,7 @@
+CONST = 1024
+volume = 1.44
+page = 100
+str = 50
+symbol = 25
+weight = 4
+print("Количество книг, помещающихся на дискету:", int(volume * CONST * CONST // (page * str * symbol * weight)))
