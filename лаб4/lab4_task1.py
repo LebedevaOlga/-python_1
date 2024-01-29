1991020b356aa35a8c7e3d108b9539c60ba974ab
LebedevaOlga <153619980+LebedevaOlga@users.noreply.github.com>
2024-01-29 15:13:04 +0300

    Лабораторная работа 4

---
+++ лаб4/lab4_task1.py
@@ -0,0 +1,13 @@
+# TODO решите задачу
+import json
+FILENAME = "input.json"
+
+
+def task() -> float:
+    with open(FILENAME) as f:
+        json_content = json.load(f)
+    sum_ = sum([value["score"] * value["weight"] for value in json_content])
+    return round(sum_, 3)
+
+
+print(task())
